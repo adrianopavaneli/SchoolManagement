87684e85c5c3b4e607b33b5f3a6d77f4870a0991
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-08-07 12:23:40 -0300

    Criacao classes Student e Teacher

---
+++ src/entities/Student.java
@@ -0,0 +1,58 @@
+package entities;
+
+/**
+ * Created byAdriano Pavaneli on 07/08/2022. 
+ * this class is resposible for keeping the
+ * track of students fees, name, grade & fees
+ * paid.
+ * 
+ */
+public class Student {
+	private int id;
+	private String name;
+	private int grade;
+	private int feesPaid;
+	private int feesTotal;
+	
+	/**
+	 * To create a new student by initializing.
+	 * @param id id ofr the student: unique.
+	 * @param name name of the student.
+	 * @param grade grade of the student.
+	 */
+	
+	
+	public Student(int id, String name, int grade) {		
+		this.feesPaid=0;
+		this.feesTotal=30000;
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+	}
+	
+	//Not going to alter student's name, student's id.
+	
+	/**
+	 * Used to update the student's grade.
+	 * @param grade new grade of the student.
+	 */
+	
+	public void setGrade(int grade) {
+		this.grade=grade;
+		
+	}
+	
+	/**
+	 * Keep adding the fees to feesPaid Field.
+	 * Add the fees to the fees paid.
+	 * The school is going receive the funds.
+	 * 
+	 * @param fees the fees that the student pays.
+	 */
+	
+	public void updateFeesPaid(int fees) {
+		feesPaid += fees;
+	}
+	
+
+}
---
+++ src/entities/Teacher.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Teacher {
+	private int id;
+	private String name;
+	private double salary;
+	
+	
+	public Teacher(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	
+
+}
